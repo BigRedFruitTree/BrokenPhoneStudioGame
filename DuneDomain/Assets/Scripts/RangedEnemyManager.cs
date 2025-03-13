377b7d93896a5464b5641cc698b152d9782c4994
Farcry5AttackersGames <farcry5attackersgames132@gmail.com>
2025-03-13 13:39:53 -0700

    Worked on RangedEnemyManager Script

--- DuneDomain/Assets/Scripts/RangedEnemyManager.cs
+++ DuneDomain/Assets/Scripts/RangedEnemyManager.cs
@@ -7,27 +7,41 @@ public class RangedEnemyManager : MonoBehaviour
 
     [Header("References")]
     public GameManager gm;
-    public PlayerController player;
+    public PlayerController Player;
     public GameObject PlayerObject;
     public GameObject RangedEnemyObject;
     public GameObject RangedEnemySpawn;
+    public Rigidbody EnemyRigidBody;
 
     [Header("Prefabs")]
     public GameObject RangedCorpsePrefab;
 
     [Header("Stats")]
-    public float Health = 5f;
+    public int Health;
+    public int MaxHealth;
+    public float Speed;
+    public bool CanTakeDamage = true;
+    public bool Dead = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Health = 5;
+        MaxHealth = 5;
+        Player = GameObject.Find("Player").GetComponent<PlayerController>();
+        PlayerObject = GameObject.Find("Player");
+        EnemyRigidBody = GetComponent<Rigidbody>();
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        Speed = 12f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gm.GameOn == true && gm.GameOver == false)
+        {
+
+        }
     }
 }
