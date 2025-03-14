944b4e5c0f41ffbe1426e75e6435f452f95bd3e4
Farcry5AttackersGames <farcry5attackersgames132@gmail.com>
2025-03-14 13:39:57 -0700

    Tried debugging the ranged enemy's speed. still a little too quick

--- DuneDomain/Assets/Scripts/RangedEnemyManager.cs
+++ DuneDomain/Assets/Scripts/RangedEnemyManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class RangedEnemyManager : MonoBehaviour
@@ -11,6 +12,7 @@ public class RangedEnemyManager : MonoBehaviour
     public GameObject PlayerObject;
     public GameObject RangedEnemyObject;
     public Rigidbody EnemyRigidBody;
+    public NavMeshAgent Agent;
 
     [Header("Prefabs")]
     public GameObject RangedCorpsePrefab;
@@ -18,7 +20,7 @@ public class RangedEnemyManager : MonoBehaviour
     [Header("Stats")]
     public int Health;
     public int MaxHealth;
-    public float Speed;
+    public float speed;
     public bool CanTakeDamage = true;
     public bool dead = false;
 
@@ -32,7 +34,7 @@ public class RangedEnemyManager : MonoBehaviour
         PlayerObject = GameObject.Find("Player");
         EnemyRigidBody = GetComponent<Rigidbody>();
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
-        Speed = 5f;
+        speed = 1f;
     }
 
     // Update is called once per frame
@@ -42,16 +44,18 @@ public class RangedEnemyManager : MonoBehaviour
         {
             if (gm.enemyMovePattern == 2 && dead == false)
             {
-                Vector3 lookDirection = (-PlayerObject.transform.position - RangedEnemyObject.transform.position).normalized;
-                EnemyRigidBody.AddForce(lookDirection * Speed);
+                Agent.destination = -Player.transform.position;
+                Vector3 lookDirection = (-PlayerObject.transform.position - -RangedEnemyObject.transform.position).normalized;
+                EnemyRigidBody.AddForce(lookDirection * speed);
             }
             else if (gm.enemyMovePattern == 1 && dead == false)
             {
+                Agent.destination = Player.transform.position;
                 Vector3 lookDirection = (PlayerObject.transform.position - RangedEnemyObject.transform.position).normalized;
-                EnemyRigidBody.AddForce(lookDirection * Speed);
+                EnemyRigidBody.AddForce(lookDirection * speed);
             }
 
-            else if (Health < 0 && dead == false)
+            if (Health <= 0 && dead == false)
             {
                 Destroy(RangedEnemyObject);
                 Instantiate(RangedCorpsePrefab, new Vector3(RangedEnemyObject.transform.position.x, RangedEnemyObject.transform.position.y - 0.59f, RangedEnemyObject.transform.position.z), Quaternion.Euler(90, 0, 0));
