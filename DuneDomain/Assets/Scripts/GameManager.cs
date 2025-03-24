663b3ad7ad42e04e1637abefe20d17a17a7b0de7
BigRedFruitTree <blaine.dooley07@gmail.com>
2025-03-23 18:26:12 -0700

    Added Basic Attack system to Boss
    -Boss picks between different attacks. The amount of attacks and how long they last for can be changed.
    -Changed Boss size to better reflect the end versions size.

--- DuneDomain/Assets/Scripts/GameManager.cs
+++ DuneDomain/Assets/Scripts/GameManager.cs
@@ -20,6 +20,7 @@ public class GameManager : MonoBehaviour
     public bool canBossEat = false;
     public bool isProcessingTarget = false;
     public int bossAttack = 0;
+    public bool bossAttacking = false;
     public NavMeshAgent bossAgent;
     public GameObject bossObject;
     public GameObject bossSpawn;
@@ -54,22 +55,23 @@ public class GameManager : MonoBehaviour
     {
         if (SceneManager.GetActiveScene().buildIndex > 0)
         {
+            bossObject.SetActive(false);
             startCycle = false;
             weaponScreen.SetActive(true);
-            if(rounds <= 0)
+            if (rounds <= 0)
             {
-               weaponKeepButton.SetActive(false);
-               weaponKeepTXT.SetActive(false);
+                weaponKeepButton.SetActive(false);
+                weaponKeepTXT.SetActive(false);
             }
-       
+
             timeUntilAppearance = 6000f;
             timeUntilEatPhase = 2000f;
-            timeUntilAttack = Random.Range(5f, 10f);
+            timeUntilAttack = Random.Range(200f, 300f);
             rounds = 1;
             spawnRange = 40f;
             meleeEnemyObject = meleeEnemyPrefab;
             rangedEnemyObject = rangedEnemyPrefab;
- 
+
             if (enemyCorpseNumber == null || enemyCorpseNumber.Length == 0) return;
 
             SetNextTarget();
@@ -77,25 +79,25 @@ public class GameManager : MonoBehaviour
             playerController = GameObject.Find("Player").GetComponent<PlayerController>();
         }
 
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(GameOn == true && GameOver == false && started == true)
-       {
+        if (GameOn == true && GameOver == false && started == true)
+        {
             meleeEnemyNumber = GameObject.FindGameObjectsWithTag("MeleeEnemy");
             enemyCorpseNumber = GameObject.FindGameObjectsWithTag("EnemyCorpse");
             rangedEnemyNumber = GameObject.FindGameObjectsWithTag("RangedEnemy");
 
-            while (meleeEnemyNumber.Length > 14) 
+            while (meleeEnemyNumber.Length > 14)
             {
-               Destroy(GameObject.FindGameObjectWithTag("MeleeEnemy"));
+                Destroy(GameObject.FindGameObjectWithTag("MeleeEnemy"));
             }
             while (rangedEnemyNumber.Length > 14)
             {
-               Destroy(GameObject.FindGameObjectWithTag("RangedEnemy"));
+                Destroy(GameObject.FindGameObjectWithTag("RangedEnemy"));
             }
 
             if (enemyCorpseNumber.Length >= rangedEnemyNumber.Length)
@@ -117,99 +119,119 @@ public class GameManager : MonoBehaviour
             }
 
             if (timeUntilAppearance > 0f)
-            { 
-              StartCoroutine("Wait");
-              timeUntilAppearance--;
+            {
+                StartCoroutine("Wait");
+                timeUntilAppearance--;
             }
 
             if (timeUntilAppearance <= 0f && timeUntilEatPhase > 0f)
             {
-               if (timeUntilAttack > 0f)
-               {
+                if (timeUntilAttack > 0f)
+                {
+                    StartCoroutine("Wait");
+                    timeUntilAttack--;
+                }
+                if (timeUntilAttack <= 0f && bossAttack == 0)
+                {
+                    timeUntilAttack = 0f;
+                    bossAttack = Random.Range(0, 4);
+                }
 
-               }
-               timeUntilAppearance = 0f;
-               bossObject.SetActive(true);
-               bossAgent.destination = playerController.transform.position;
-               StartCoroutine("Wait");
-               timeUntilEatPhase--;
+                if (bossAttack == 1)
+                {
+                    StartCoroutine("WaitAttack1");
+                }
+                if (bossAttack == 2)
+                {
+                    StartCoroutine("WaitAttack2");
+                }
+                if (bossAttack == 3)
+                {
+                    StartCoroutine("WaitAttack3");
+                }
+
+                timeUntilAppearance = 0f;
+                bossObject.SetActive(true);
+                bossAgent.destination = playerController.transform.position;
+                StartCoroutine("Wait");
+                timeUntilEatPhase--;
             }
 
-           if (timeUntilEatPhase <= 0f)
-           {
-              timeUntilEatPhase = 0f;
-              if(enemyCorpseNumber.Length > 0)
-              {
-                 if (bossEating == false)
-                 {
-                     SetNextTarget();
-                 }
-                 if (!isProcessingTarget && currentTarget != null && !bossAgent.pathPending && bossAgent.remainingDistance <= bossAgent.stoppingDistance)
-                 {
-                     bossEating = true;
-                 }
-                 if (!isProcessingTarget && currentTarget != null && bossEating == true && !bossAgent.pathPending && bossAgent.remainingDistance <= bossAgent.stoppingDistance)
-                 {
-                     StartCoroutine("WaitForEating");
-                     bossEating = false;
-                 }
-              }
-              else
-              {
-                 bossEating = false;
-              }
-           }
+            if (timeUntilEatPhase <= 0f)
+            {
+                timeUntilEatPhase = 0f;
+                if (enemyCorpseNumber.Length > 0)
+                {
+                    if (bossEating == false)
+                    {
+                        SetNextTarget();
+                    }
+                    if (!isProcessingTarget && currentTarget != null && !bossAgent.pathPending && bossAgent.remainingDistance <= bossAgent.stoppingDistance)
+                    {
+                        bossEating = true;
+                    }
+                    if (!isProcessingTarget && currentTarget != null && bossEating == true && !bossAgent.pathPending && bossAgent.remainingDistance <= bossAgent.stoppingDistance)
+                    {
+                        StartCoroutine("WaitForEating");
+                        bossEating = false;
+                    }
+                }
+                else
+                {
+                    bossEating = false;
+                }
+            }
 
-           if(bossEating == false && timeUntilEatPhase <= 0f && enemyCorpseNumber.Length == 0)
-           {
-              if(rounds > 0 && startCycle == false)
-              {
-                 StartCoroutine("WaitBossAway");
-                 StartCoroutine("WaitWeaponScreen");
-              }
-           }
+            if (bossEating == false && timeUntilEatPhase <= 0f && enemyCorpseNumber.Length == 0)
+            {
+                if (rounds > 0 && startCycle == false)
+                {
+                    StartCoroutine("WaitBossAway");
+                    StartCoroutine("WaitWeaponScreen");
+                }
+            }
 
-           if(bossEating == false && timeUntilEatPhase <= 0f && startCycle == true && enemyCorpseNumber.Length == 0)
-           {
-              if(meleeEnemyNumber.Length < 15)
-              {
-                 SpawnMelee(rounds);
-              } 
-              if(rangedEnemyNumber.Length < 15 && rounds >= 2)
-              {
-                 SpawnRanged(rounds);
-              }
-              timeUntilAppearance = 6000f;
-              timeUntilEatPhase = 2000f;
-              startCycle = false;
-             
-           }
+            if (bossEating == false && timeUntilEatPhase <= 0f && startCycle == true && enemyCorpseNumber.Length == 0)
+            {
+                if (meleeEnemyNumber.Length < 15)
+                {
+                    SpawnMelee(rounds);
+                }
+                if (rangedEnemyNumber.Length < 15 && rounds >= 2)
+                {
+                    SpawnRanged(rounds);
+                }
+                timeUntilAppearance = 6000f;
+                timeUntilEatPhase = 2000f;
+                startCycle = false;
 
-           if (playerController.health <= 0)
-           {
+            }
+
+            if (playerController.health <= 0)
+            {
                 GameOn = false;
                 GameOver = true;
                 deathScreen.SetActive(true);
                 Time.timeScale = 0;
-           }
+            }
 
-       }
+        }
     }
 
     public void SpawnMelee(int numberToSpawn)
     {
         for (int i = 0; i < numberToSpawn; i++)
         {
-             Instantiate(meleeEnemyPrefab, GenerateSpawnPos(), meleeEnemyPrefab.transform.rotation);
+            Instantiate(meleeEnemyPrefab, GenerateSpawnPos(), meleeEnemyPrefab.transform.rotation);
         }
     }
     public void SpawnRanged(int numberToSpawn)
     {
-        
+
         for (int i = 0; i < numberToSpawn; i++)
         {
-             Instantiate(rangedEnemyPrefab, GenerateSpawnPos(), rangedEnemyPrefab.transform.rotation);
-        } 
+            Instantiate(rangedEnemyPrefab, GenerateSpawnPos(), rangedEnemyPrefab.transform.rotation);
+        }
     }
 
     public Vector3 GenerateSpawnPos()
@@ -223,20 +245,20 @@ public class GameManager : MonoBehaviour
 
     public void ChooseWeapon(int chosenWeapon)
     {
-        if(chosenWeapon == 1)
+        if (chosenWeapon == 1)
         {
-            if(rounds > 1)
-              startCycle = true;
+            if (rounds > 1)
+                startCycle = true;
 
             weaponScreen.SetActive(false);
             weapon = chosenWeapon;
             GameOn = true;
             StartCoroutine("Wait");
         }
-        if(chosenWeapon == 2)
+        if (chosenWeapon == 2)
         {
-            if(rounds > 1)
-             startCycle = true;
+            if (rounds > 1)
+                startCycle = true;
 
             playerController.drawSpeed = 100f;
             weaponScreen.SetActive(false);
@@ -279,19 +301,19 @@ public class GameManager : MonoBehaviour
 
     public void KeepWeapon()
     {
-        if(weapon == 1)
+        if (weapon == 1)
         {
-            if(rounds > 1)
-             startCycle = true;
+            if (rounds > 1)
+                startCycle = true;
 
             weaponScreen.SetActive(false);
             GameOn = true;
             StartCoroutine("Wait");
         }
-        if(weapon == 2)
+        if (weapon == 2)
         {
-            if(rounds > 1)
-             startCycle = true;
+            if (rounds > 1)
+                startCycle = true;
 
             playerController.drawSpeed = 100f;
             weaponScreen.SetActive(false);
@@ -337,7 +359,7 @@ public class GameManager : MonoBehaviour
 
     public void SetNextTarget()
     {
-        if (enemyCorpseNumber.Length == 0) 
+        if (enemyCorpseNumber.Length == 0)
             return;
 
         currentTarget = GetNearestTarget();
@@ -382,7 +404,24 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(0.1f);
         started = true;
     }
-
+    IEnumerator WaitAttack1()
+    {
+        yield return new WaitForSeconds(2f);
+        bossAttack = 0;
+        timeUntilAttack = Random.Range(200f, 300f);
+    }
+    IEnumerator WaitAttack2()
+    {
+        yield return new WaitForSeconds(2f);
+        bossAttack = 0;
+        timeUntilAttack = Random.Range(200f, 300f);
+    }
+    IEnumerator WaitAttack3()
+    {
+        yield return new WaitForSeconds(2f);
+        bossAttack = 0;
+        timeUntilAttack = Random.Range(200f, 300f);
+    }
     IEnumerator WaitBossAway()
     {
         yield return new WaitForSeconds(2f);
@@ -391,11 +430,12 @@ public class GameManager : MonoBehaviour
     }
 
     IEnumerator WaitForEating()
-    {   isProcessingTarget = true;
+    {
+        isProcessingTarget = true;
         yield return new WaitForSeconds(5f);
         if (currentTarget != null)
         {
-           Destroy(currentTarget);
+            Destroy(currentTarget);
         }
         currentTarget = null;
         SetNextTarget();
