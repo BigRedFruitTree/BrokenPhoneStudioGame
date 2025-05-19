b4b6ec45c3a761a4220af3b4a34f3ee53fe97aca
Farcry5AttackersGames <farcry5attackersgames132@gmail.com>
2025-05-19 13:40:53 -0700

    Tutorial Scene & new Waitdraw couretine

--- DuneDomain/Assets/Scripts/PlayerController.cs
+++ DuneDomain/Assets/Scripts/PlayerController.cs
@@ -610,7 +610,7 @@ public class PlayerController : MonoBehaviour
 
     IEnumerator WaitDraw()
     {
-         yield return new WaitForSeconds(0.5f);
+         yield return new WaitForSeconds(0.1f);
          drawSpeed -= Time.deltaTime * 60;
     }
 
